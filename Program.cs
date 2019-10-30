12dcc9aa8957409f703d2509ee60f329e1583f2c
Geoffrey Miller <gmiller@vailsys.com>
2019-10-30 13:40:26 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using com.freeclimb.api;
+using com.freeclimb.api.queue;
+
+namespace DequeueMember {
+  class Program {
+    static string getFreeClimbAccountId () {
+      return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+    }
+
+    static string getFreeClimbAccountToken () {
+      return System.Environment.GetEnvironmentVariable("AUTH_TOKEN");
+    }
+
+    static void Dequeue(string queueId, string callId = "") {
+      // Create FreeClimbClient object
+      FreeClimbClient client = new FreeClimbClient (getFreeClimbAccountId (),
+        getFreeClimbAccountToken ());
+
+      // Invoke method to update the QueueMeber to dequeue it from the queue
+      QueueMember queueMember = client.getQueuesRequester.updateQueueMember(queueId, callId);
+
+    }
+    static void Main (string[] args) {
+      string queueId = "";
+      string callId = "";
+
+      Dequeue(queueId, callId);
+    }
+  }
+}
